91b37047760ba2c759bdcde8ba27df50f1053d7e
Aimee <aimeedowell24@gmail.com>
2022-10-13 11:09:14 +0100

    Complete step 4

--- src/main/java/com/lbg/App.java
+++ src/main/java/com/lbg/App.java
@@ -1,5 +1,6 @@
 package com.lbg;
 
+
 /**
  * Hello world!
  *
@@ -9,5 +10,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        VATCalculator calc = new VATCalculator();
+
+        System.out.println( calc.calculateVATToPay(10, 10));
+
     }
 }
